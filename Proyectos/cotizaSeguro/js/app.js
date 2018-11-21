5a01b831f604e534216605fda63a5cb242d42afb
Carlos3dg <carlosoo3dg@gmail.com>
2018-11-21 00:34:04 -0600

    Add a validate message for form fields and get the insucrance price value

--- Proyectos/cotizaSeguro/js/app.js
+++ Proyectos/cotizaSeguro/js/app.js
@@ -1,14 +1,79 @@
 //Global variables
 const select = document.getElementById('anio');
+const form = document.getElementById('cotizar-seguro');
 
 // Class for cotizar seguro
-function Seguro() {
+function Seguro(marca, anio, tipo) {
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo;
+}
 
+//Prototype to calculate the Insurance
+Seguro.prototype.cotizarSeguro = function() {
+    //Base price
+    const precioBase = 2000;
+    let cantidad; //The variable to store the final price 
+
+    /*
+        1 = americano -- 1.15;
+        2 = asiatico -- 1.05;
+        3 = europeo -- 1.35;
+    */
+   //Detemine the type of marca
+    switch(this.marca) {
+        case '1':
+            cantidad = precioBase * 1.15;
+            break;
+        case '2':
+            cantidad = precioBase * 1.05;
+            break;
+        case '3':
+            cantidad = precioBase * 1.35;
+            break;
+    }
+    
+    //Calculate the price according to the year
+    /*
+        Every year in the past according to the present year is 3% less over the price in the present
+    */
+   const diferencia = new Date().getFullYear() - this.anio;
+   cantidad -= (cantidad * (diferencia * 0.03));
+
+   //Calculate the price according to the insurance type
+   /*
+        If insurance is basic the price increase 30% more;
+        If insurance is full the price increase 50% more;
+   */
+    if (this.tipo === 'basico') {
+        cantidad *= 1.30;
+    } else {
+        cantidad *= 1.50;
+    }
+
+    return cantidad;
 }
 
 // Class for UI
 function UI() {
 
+}
+
+//Prototype used to show a message when the form is submited
+UI.prototype.showMessage = function (mensaje, tipo) {
+    const div = document.createElement('div');
+    if (tipo === 'error') {
+        div.classList.add('mensaje', 'error');
+    } else {
+        div.classList.add('mensaje', 'correcto');
+    }
+
+    div.innerHTML = mensaje;
+    form.insertBefore(div, document.querySelector('.form-group'));
+   
+    setTimeout(function() {
+        document.querySelector('.mensaje').remove();
+    }, 3000)
 }
 
 //Create <option> tags in <select>
@@ -24,7 +89,6 @@ for(let i = max; i >= min; i--) {
 
 //Event Listeners
 //Submit event in form:
-const form = document.getElementById('cotizar-seguro');
 form.addEventListener('submit', function(e){
     e.preventDefault();
     //Get marca: Americano, Asiatico, Europeo..
@@ -41,11 +105,13 @@ form.addEventListener('submit', function(e){
     const interfaz = new UI();
     //Validate that all fields are completed
     if(marcaValue === '' || anioValue === '' || typeInsurance === ''){
-        console.log('Campos vacions');
+        interfaz.showMessage('Error, faltan datos por completar, favor de volver a intentar', 'error');
     } else {
-        console.log('Campos completos');
-    }
+        const seguro = new Seguro(marcaValue, anioValue, typeInsurance);
 
+        const cantidad = seguro.cotizarSeguro()
+    }
+    
 });
 
 
