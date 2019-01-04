0cc30455f71a02393ac04d20fef6dd8f72da766b
Carlos3dg <carlosoo3dg@gmail.com>
2019-01-04 00:42:14 -0600

    Add empleados program

--- AjaxAndJson/AJAX&JSON/app.js
+++ AjaxAndJson/AJAX&JSON/app.js
@@ -32,3 +32,41 @@ empleadoBtn.addEventListener('click', function() {
     // 4. Send request
     xhr.send();
 })
+
+//Program to onload employments from empleados.json
+const empleadosBtn = document.getElementById('boton2');
+
+//Event litener to onload data from empleados.json
+empleadosBtn.addEventListener('click', function() {
+    // 1. Creation of the XMLHttp object
+    const xhr = new XMLHttpRequest();
+
+    // 2. Open the connection
+    xhr.open('GET', 'empleados.json', true);
+
+    // 3. Onload the request
+    xhr.onload = function() {
+        // Evalution if the onload is succesfully
+        if (xhr.status===200) {
+            const empleados = JSON.parse(this.responseText);
+
+            let htmlEmpleados = "";
+
+            empleados.forEach(function(empleado){
+                htmlEmpleados += `
+                    <ul>
+                        <li>ID: ${empleado.id}</li>
+                        <li>Nombre: ${empleado.nombre}</li>
+                        <li>Empresa: ${empleado.empresa}</li>
+                        <li>Trabajo: ${empleado.trabajo}</li>
+                    </ul>
+                `;
+            });
+
+           document.getElementById('empleados').innerHTML = htmlEmpleados;
+        }
+    }
+
+    // 4. Send Request
+    xhr.send()
+})
