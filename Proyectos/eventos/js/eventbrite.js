b2a1cc48bffe8a6b17da2dff84b6d883832ae149
Carlos Obregon <cobregon@besserenergy.com>
2019-02-13 16:59:55 -0600

    Add options in select element in the events project

--- Proyectos/eventos/js/eventbrite.js
+++ Proyectos/eventos/js/eventbrite.js
@@ -5,6 +5,9 @@ class API {
     }
     //Method to get the events according with the input data
     async loadEventRequest() {
+        const response =  await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.oauthToken}`);
+        const categories = await response.json();
 
+        return categories;
     }
 }
--- Proyectos/eventos/js/interfaz.js
+++ Proyectos/eventos/js/interfaz.js
@@ -5,10 +5,24 @@ class UI {
     }
     //Method to be executed when the app is initiating
     init() {
-
+        this.eventDesignResult();
     }
     //Method to design the event result search according with the input data
     eventDesignResult() {
-        const eventObject = api.loadEventRequest();
+        const eventObject = api.loadEventRequest()
+            .then(result => {
+                //Get categories as objects in an array
+                const categories = result.categories;
+                //Get select element
+                const select = document.getElementById('listado-categorias');
+                //Create every option inside select element with the respective categories
+                categories.forEach((categorie) => {
+                    const option = document.createElement('option');
+                    option.value = categorie.id;
+                    option.appendChild(document.createTextNode(categorie.name));
+                    select.appendChild(option);
+                });
+
+            });
     }
 }
