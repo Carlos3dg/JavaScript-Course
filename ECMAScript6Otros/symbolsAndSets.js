39961994e220edfd20142f56551722f232dcc98a
Carlos3dg <carlosoo3dg@gmail.com>
2019-08-13 22:05:10 -0500

    Add Maps topic

--- ECMAScript6Otros/symbolsAndSets.js
+++ ECMAScript6Otros/symbolsAndSets.js
@@ -31,7 +31,7 @@ for (i in person) {
 } /* This just print the name property, but no the Symbol */
 
 
-// Sets
+// SETS
 /*This are similar to the array objects, they contain elements but with the exception that those elements can no be repeted inside the set, this can be seen like an array without repeated values*/
 
 //Creation
@@ -69,3 +69,52 @@ console.log(users) //Set(3) {'pablo', 'juan', 'lizeth'}
 //To delete a value from it
 users.delete('pablo');
 console.log(users);
+
+//To get the length
+console.log(users.size); //2 Because of the delete that has just happen
+
+//To clear the set
+users.clear();
+console.log(users); // Set(0) {}
+
+//If we wanted to turn our Set into an Arrya we do it as follow:
+const phones = new Set([8186, 8245, 8365]);
+//Make the transformation
+const phonesArray = [... phones];
+console.log(phonesArray) // [8186, 8245, 8365]
+
+// MAPS
+/* These are similar to JavaScript objects, they contain a key property and a value but with the difference that they can not have repeated properties*/
+
+//Creation 
+const grades = new Map();
+//Adding values
+grades.set('Math', 93);
+grades.set('Science', 85);
+console.log(grades); // Map(2) {'Math' => 93, 'Science' => 85}
+
+//To print a specific value
+console.log(grades.get('Math')) // 93
+
+//To get the length of our map
+console.log(grades.size); //2
+
+//To evaluate if the map has a determine property
+console.log(grades.has('Science')); // True, due to this property is in the map
+
+//To delete a property 
+grades.delete('Math');
+console.log(grades); //Map(1) {'Science' => 85} Now the Map property doesn't exists
+
+//To clear the map
+grades.clear();
+console.log(grades); //Map(0) {}
+
+//To add values in the definition of our map
+const finalGrades = new Map(
+     [
+          ['Math', 93],
+          ['Science', 85]
+     ]
+);
+console.log(finalGrades); //Map(2) {'Math' => 93, 'Science' => 85}
